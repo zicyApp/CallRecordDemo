28f27c745c18bb19ca32ed9a552dec5b26834f09
zicyApp <polovitzer.robert@gmail.com>
2017-08-16 10:02:55 -0400

    when you restart your device your app will working stop, recording stop, its become useless

--- RecordCall/app/src/main/java/app/test/com/recordcall/DeviceAdminDemo.java
+++ RecordCall/app/src/main/java/app/test/com/recordcall/DeviceAdminDemo.java
@@ -12,6 +12,11 @@ public class DeviceAdminDemo extends DeviceAdminReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
+
+        context.stopService(new Intent(context, TService.class));
+        Intent myIntent = new Intent(context, TService.class);
+        context.startService(myIntent);
+
     }
 
     public void onEnabled(Context context, Intent intent) {
