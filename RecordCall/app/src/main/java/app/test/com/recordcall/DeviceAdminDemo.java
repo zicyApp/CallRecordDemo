a8d09c8b97f88e92aeae2dd44d4f4e9bb87354ce
zicyApp <polovitzer.robert@gmail.com>
2017-08-16 09:48:46 -0400

    Initial commit

---
+++ RecordCall/app/src/main/java/app/test/com/recordcall/DeviceAdminDemo.java
@@ -0,0 +1,22 @@
+package app.test.com.recordcall;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by robertpolovitzer on 2017-08-16.
+ */
+
+public class DeviceAdminDemo extends DeviceAdminReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    public void onEnabled(Context context, Intent intent) {
+    };
+
+    public void onDisabled(Context context, Intent intent) {
+    };
+}
